674c6aaf136f24cdbcdcfa17d55589d05f4689b6
Exam 42 <exam-no-reply@42.fr>
2021-12-11 13:14:51 +0300

    +;

--- microshell/main.c
+++ microshell/main.c
@@ -4,6 +4,7 @@
 
 char**	cmd;
 char**	ep;
+char	delim = ';';
 
 void	run_cmd()
 {
@@ -15,13 +16,28 @@ pid_t	fork_cmd()
 	pid_t	pid = fork();
 	if ( pid == 0 )
 		run_cmd();
-	else if ( pid > 0)
-		return pid;
-	else
+	if ( pid < 0 )
 		exit_me(ERROR_FATAL);
+	return pid;
+}
+char** skip_cmd(char* av[])
+{
+	while( *av && strcmp(*av, ";") != 0 && strcmp(*av, "|") )
+		av++;
+	delim = ';';
+	if (*av) {
+		delim = **av;
+		*av = NULL;
+		av++;
+	}
+	return av;
 }
 int	main(int argc, char* argv[], char* envp[])
 {
-	cmd = ++argv;
-	waitpid(fork_cmd(), NULL, 0);
+	(void) argc;
+	ep = envp;
+	for (cmd = ++argv; *argv; cmd = argv ){
+		argv = skip_cmd(argv);
+		waitpid(fork_cmd(), NULL, 0);
+	}
 }
--- microshell/t.sh
+++ microshell/t.sh
@@ -1,8 +1,9 @@
 make || exit 1
 
 ./aa /bin/echo
-./aa /bin/echo tst
-./aa /bin/sleep 2
+./aa /bin/echo 1
+./aa /bin/sleep 2; /bin/echo 2
+./aa /bin/echo 3; /bin/echo 4;
 
 ./aa asdf
 ./aa main.c

7a176d80bca3b99e93c740d4f508d5a37344906f
Exam 42 <exam-no-reply@42.fr>
2021-12-11 13:55:48 +0300

    t.sh += subject cases

--- microshell/t.sh
+++ microshell/t.sh
@@ -7,5 +7,8 @@ make || exit 1
 ./aa /bin/sleep 5 "|" /bin/echo 5 ";"
 ./aa /bin/echo 6 "|" /bin/cat
 
+./aa /bin/ls "|" /usr/bin/grep microshell ";" /bin/echo i love my microshell
+./aa /bin/echo WOOT "; /bin/echo NOPE;" "; ;" ";" /bin/echo YEAH
+
 ./aa asdf
 ./aa main.c

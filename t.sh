2229a83fba345297a820ca726d8c21ed311eb9ab
Carmine Watcher <cwatcher@student.21-school.r>
2021-12-13 21:06:52 +0300

    t.sh -= sleep

--- t.sh
+++ t.sh
@@ -2,11 +2,15 @@ make || exit 1
 
 ./aa /bin/echo
 ./aa /bin/echo 1
-./aa /bin/sleep 1 ";" /bin/echo 2
-./aa /bin/echo 3 ";" /bin/echo 4 ";"
-./aa /bin/sleep 5 "|" /bin/echo 5 ";"
+
+./aa /bin/echo 1 ";" /bin/echo 2
+./aa /bin/echo 3 ";"
+./aa ";" /bin/echo 4
+./aa /bin/echo 5 ";" ";" /bin/echo 6
+
+./aa /bin/echo a "|" /usr/bin/wc -w
+./aa /bin/echo a b "|" /usr/bin/wc -w "|" /usr/bin/sed
 ./aa /bin/echo 6 "|" /bin/cat
-./aa ";" /bin/echo 7
 ./aa cd / ";" /bin/pwd
 
 ./aa /bin/ls "|" /usr/bin/grep microshell ";" /bin/echo i love my microshell

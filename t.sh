b29d97cd09088002163ef7229cec768fbbeb0c63
Carmine Watcher <cwatcher@student.21-school.r>
2021-12-14 14:17:02 +0300

    t.sh += "ok!"

--- t.sh
+++ t.sh
@@ -1,22 +1,21 @@
 make || exit 1
 
-./aa /bin/echo
-./aa /bin/echo 1
+tt.sh './aa /bin/echo'
+tt.sh '/bin/echo 1'
 
-./aa /bin/echo 1 ";" /bin/echo 2
-./aa /bin/echo 3 ";"
-./aa ";" /bin/echo 4
-./aa /bin/echo 5 ";" ";" /bin/echo 6
+tt.sh '/bin/echo 1 2 ; /bin/echo'
+tt.sh '/bin/echo 3 ;'
+tt.sh '; /bin/echo 4'
+tt.sh '/bin/echo 5 ; ; /bin/echo 6 ;'
+tt.sh '; /bin/echo 7 ; ; /bin/echo 8 ;'
 
-./aa /bin/echo a "|" /usr/bin/wc -w
-./aa /bin/echo a b "|" /usr/bin/wc -w "|" /usr/bin/sed
-./aa /bin/echo 6 "|" /bin/cat
-./aa cd / ";" /bin/pwd
+tt.sh '/bin/ls . | /usr/bin/wc -w'
+tt.sh '/usr/bin/seq 21 | /usr/bin/paste -s -d"*" - | /usr/bin/bc'
 
-./aa /bin/ls "|" /usr/bin/grep microshell ";" /bin/echo i love my microshell
-./aa /bin/echo WOOT "; /bin/echo NOPE;" "; ;" ";" /bin/echo YEAH
+tt.sh 'cd /usr/bin ; /bin/pwd'
 
+tt.sh '/bin/ls | /usr/bin/grep aa ; /bin/echo i love my aa'
+tt.sh '/bin/echo WOOT ; /bin/echo NOPE; ; ; ; /bin/echo YEAH'
 
-
-./aa asdf
+./aa nothing
 ./aa main.c
